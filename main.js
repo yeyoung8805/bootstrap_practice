0da5e18e28919e3def22666c128322a61899064e
“yeyoung8805” <yyk8805@naver.com>
2022-08-19 17:54:16 +0900

    how to use Tooltip

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const emailInputEl = document.querySelector('#exampleInputEmail1');
-const modalEl = document.querySelector('#exampleModal');
+//Tooltop 직접 초기화
 
-modalEl.addEventListener('shown.bs.modal', function() {
-  emailInputEl.focus();
-})
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
