2b09715f10773d3741db220c50fd7776b276e856
82227496 <yyk8805@naver.com>
2022-08-24 11:05:44 +0900

    customize modal settings

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //dropdown을 사용하기 위한 초기화
 import Dropdown from "bootstrap/js/dist/dropdown"; //bootstrap 안의 dropdown만 불러온다(성능 최적화를 위해)
+import Modal from "bootstrap/js/dist/modal";
 
 const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
 [...dropdownElementList].map(
@@ -7,7 +8,6 @@ const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
 );
 
 //modal 을 사용하기 위한 초기화
-import Modal from "bootstrap/js/dist/modal";
-const myModal = new Modal(document.getElementById("myModal"), options);
+new Modal(document.querySelector("#exampleModal"));
 // or
 // const myModalAlternative = new bootstrap.Modal("#myModal", options);
