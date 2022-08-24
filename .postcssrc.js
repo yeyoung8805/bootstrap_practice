95b831805e96207e468d42538d4e784d81656797
82227496 <yyk8805@naver.com>
2022-08-24 13:52:25 +0900

    difference between ESM and CommonJS

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+//ESM(ECMA Script Module) : import, export를 사용하는 기존 JS 개념 (브라우저 상에서 작동하는 개념)
+//하지만 Node.js에서는 기본적으로 ESM 이 아닌 CommonJS 라는 방식을 지원한다. (Node.js 상에서 작동하는 개념)
+//import 대신에 require() 사용
+//export 대신에 module.exports 사용
+
+//import
+require();
+
+//export
+module.exports;
