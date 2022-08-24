71ed0f9e074ac49046774864ee67575a371b9463
82227496 <yyk8805@naver.com>
2022-08-24 10:46:14 +0900

    remove unnecessary code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Dropdown from "bootstrap/js/dist/dropdown"; //bootstrap 안의 dropdown만 불러온다(성능 최적화를 위해)
 
 const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
-const dropdownList = [...dropdownElementList].map(
+[...dropdownElementList].map(
   (dropdownToggleEl) => new Dropdown(dropdownToggleEl)
 );
