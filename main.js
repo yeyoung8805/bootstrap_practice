0aca629cbf159c9c58b2703ca7b26e8ded9e65dd
82227496 <yyk8805@naver.com>
2022-08-24 11:04:10 +0900

    initialize to use modal

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+//dropdown을 사용하기 위한 초기화
 import Dropdown from "bootstrap/js/dist/dropdown"; //bootstrap 안의 dropdown만 불러온다(성능 최적화를 위해)
 
 const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
 [...dropdownElementList].map(
   (dropdownToggleEl) => new Dropdown(dropdownToggleEl)
 );
+
+//modal 을 사용하기 위한 초기화
+import Modal from "bootstrap/js/dist/modal";
+const myModal = new Modal(document.getElementById("myModal"), options);
+// or
+// const myModalAlternative = new bootstrap.Modal("#myModal", options);
