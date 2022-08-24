ccd3906edd609c999ac7eee0c74253125df7a13c
82227496 <yyk8805@naver.com>
2022-08-24 11:10:21 +0900

    add backdrop option not to close when clicking bg

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
 );
 
 //modal 을 사용하기 위한 초기화
-new Modal(document.querySelector("#exampleModal"));
+new Modal(document.querySelector("#exampleModal"), {
+  backdrop: "static", //배경 부분 클릭해도 닫히지 않게 옵션 설정함
+});
 // or
 // const myModalAlternative = new bootstrap.Modal("#myModal", options);
